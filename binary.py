cc3fbeab9d8bc76b321b4560eb7de794459469b3
Sachin_0310 <sachingnanesh0310@gmail.com>
2026-06-18 08:48:20 +0530

    Add files via upload

---
+++ binary.py
@@ -0,0 +1,20 @@
+# Binary Search without user input
+
+arr = [10, 20, 30, 40, 50, 60, 70]
+key = 40
+
+low = 0
+high = len(arr) - 1
+
+while low <= high:
+    mid = (low + high) // 2
+
+    if arr[mid] == key:
+        print("Element found at position", mid + 1)
+        break
+    elif arr[mid] < key:
+        low = mid + 1
+    else:
+        high = mid - 1
+else:
+    print("Element not found")
